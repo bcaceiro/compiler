31471ef33e34a7ef99e77e333e5243f7eef72f4d
davidfpc <davidcardopinto@hotmail.com>
2014-05-28 18:58:14 +0100

    já imprime somas e subtracoes

--- generateCode.c
+++ generateCode.c
@@ -21,11 +21,18 @@ char* generateCode(Node* ast){
     if(ast!=NULL){
         //printf("LOLADA:%s\n",NODE_STRING[ast->n_type]);
         //printf("DEBUG:%s\n\t%s\n",NODE_STRING[ast->n_type],NODE_TYPE_NAMES[ast->type]);
+        if(ast->n_type == NODE_PROGRAM){
+            printf("declare i32 @printf(i8* noalias nocapture, ...)\n@.printInt = private constant [4 x i8] c\"%%d\\0A\\00\"\n@.printBool = private constant [4 x i8] c\"%%s\\0A\\00\"\n\n");
+        }
         if(ast->n_type == NODE_METHODDECL){
+            varIndex = 0;
             insideFunction = TRUE;
             tabs++;
             if(strcmp(ast->id->id,"main")==0){
-                printf("\ndefine i32 @main(i32 %%argc, i8** %%argv) {\n\tret i32 0x1\n}\n");
+                printf("\ndefine i32 @main(i32 %%argc, i8** %%argv) {\n");
+                generateCode(ast->n2->n1);
+                generateCode(ast->n3->n1);
+                printf("\tret i32 1\n}\n");//\tret i32 0x1\n}\n");
                 //printf("\tret i32 0\n}\n");
             }
             else
@@ -45,7 +52,7 @@ char* generateCode(Node* ast){
                     printf("){\n");
                     generateCode(ast->n2->n1);
                     generateCode(ast->n3->n1);
-                    printf("\tret i1 1; MERDAS, POR ISTO BEM SENÃO DÁ MERDA\n}\n");
+                    printf("\tret i1 1\n}\n");
                 }
             tabs--;
             insideFunction = FALSE;
@@ -54,7 +61,10 @@ char* generateCode(Node* ast){
             if(insideFunction == FALSE ){
                 aux = ast->id;
                 while(aux!=NULL){
-                    printf("@%s = global %s\n",aux->id,SYMBOLS_TYPE_SIZE[ast->type]);
+                    if(ast->type == TYPE_INT || ast->type == TYPE_BOOL)
+                        printf("@%s = global %s 0\n",aux->id,SYMBOLS_TYPE_SIZE[ast->type]);
+                    else
+                        printf("@%s = global %s null\n",aux->id,SYMBOLS_TYPE_SIZE[ast->type]);
                     aux = aux->next;
                 }
             }
@@ -71,27 +81,38 @@ char* generateCode(Node* ast){
         }
         else if(ast->n_type == NODE_STORE){
             generateCode(ast->n3);
-            exit(-1);
             //printf("%s %%%s",SYMBOLS_TYPE_SIZE[ast->type],ast->id->id);
         }
-        else if(ast->n_type == NODE_PLUS){
+        else if(ast->n_type == NODE_PLUS || ast->n_type == NODE_MINUS){
             strcpy(value1,generateCode(ast->n1));
             strcpy(value2,generateCode(ast->n2));
             printTabs(tabs);
             varIndex++;
-            printf("%%%d = add %s %%%s %%%s\n",varIndex,SYMBOLS_TYPE_SIZE[ast->type],value1,value2);   //<result> = add <ty> <op1>, <op2>
-        }
-        else if(ast->n_type == NODE_INTLIT){
-            printTabs(tabs);
-            varIndex++;
-            printf("%%%d = add i32 0 %s\n",varIndex,ast->value);
-            sprintf(returnValue,"%d",varIndex);
+            printf("%%%d = %s %s %s , %s\n",varIndex,CODE_OPERATOR_STRING[ast->n_type],SYMBOLS_TYPE_SIZE[ast->type],value1,value2);   //<result> = add <ty> <op1>, <op2>
+            sprintf(returnValue,"%%%d",varIndex);
             return returnValue;
         }
+        else if(ast->n_type == NODE_INTLIT){
+            //is a decimal
+            if(*(ast->value)!='0'){
+                sprintf(returnValue,"%d",atoi(ast->value));
+                return returnValue;
+            }
 
-
-
-
+            int len = strlen(ast->value);
+            //if it is a hexadecimal
+            if(len>0 && *(ast->value+1) == 'x'){
+                sprintf(returnValue,"%ld",strtol((ast->value+2),NULL,16));
+                return returnValue;
+            }
+            sprintf(returnValue,"%ld",strtol(ast->value,NULL,8));
+            return returnValue;
+        }
+        else if(ast->n_type == NODE_PRINT){
+            printTabs(tabs);
+            //fazer ifs
+            printf("call i32 (i8*, ...)* @printf(i8* getelementptr inbounds ([4 x i8]* @.printInt, i32 0, i32 0), i32 %s)\n",generateCode(ast->n1));
+        }
 
         generateCode(ast->next);
     }
@@ -99,4 +120,4 @@ char* generateCode(Node* ast){
 
 }
 
-#endif
+            #endif
--- tests.sh
+++ tests.sh
@@ -3,11 +3,11 @@ bash compile.bash
 
 NUM_TESTS=34
 echo "Running test.java ( enunciado )"
-./ijparser < tests/test.java # > out.ll
-#llc out.ll
-#gcc -o out out.s
-#./out;
-#echo $?
+./ijparser < tests/test.java  > out.ll
+llc out.ll
+gcc -o out out.s
+./out;
+echo $?
 #for i in $(eval echo "{1..$NUM_TESTS}") ; do
 #    echo "Running test $i"
 #    ./ijparser < tests/test$i.txt
